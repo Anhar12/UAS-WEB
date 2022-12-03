9f744bec0ecda79e09d63df08d71f60d76e07f29
Anhar Khoirun Najib <92861249+Anhar12@users.noreply.github.com>
2022-12-03 21:23:12 +0800

    Update koneksi.php

--- PA-WEB-main/koneksi.php
+++ PA-WEB-main/koneksi.php
@@ -1,12 +1,8 @@
 <?php 
-    $host = "localhost";
-    $username = "root";
-    $password = "";
-
-    // Sesuaikan dengan nama Database yang ada di phpmyadmin local
-    // Jika download Database dari Github maka 
-    // tinggal Import Database saja tidak perlu mengubah nama Database
-    $database = "pa web";
+    $host = "sql305.epizy.com";
+    $username = "epiz_33027576";
+    $password = "JuhAaTwWHWJ0";
+    $database = "epiz_33027576_pweb";
 
     $conn = mysqli_connect($host, $username, $password, $database);
 
